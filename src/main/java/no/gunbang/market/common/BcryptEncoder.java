10df2306a9c4f8399ca8653956b25bf6db5ad98b
lcy <plmokn48@daum.net>
2025-04-05 22:54:35 +0900

    🎉 add: bcrypt 비밀번호 인코더(개발용)

---
+++ src/main/java/no/gunbang/market/common/BcryptEncoder.java
@@ -0,0 +1,11 @@
+package no.gunbang.market.common;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptEncoder {
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("secret"));
+    }
+
+}
